36fcdfeea4b7a65588d9963a85ea1d11f08e8b3f
Rivaldiananto <rivaldiananto@gmail.com>
2024-05-30 02:28:09 +0700

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,25 +1,12 @@
 #include <openssl/evp.h>
-#include <openssl/ec.h>
-#include <openssl/sha.h>
-#include <openssl/ripemd.h>
 #include <iostream>
 #include <iomanip>
 #include <sstream>
 #include <vector>
 
-std::string to_hex(const unsigned char* str, int len) {
-    std::stringstream ss;
-    ss << std::hex;
-    for (int i = 0; i < len; ++i)
-        ss << std::setw(2) << std::setfill('0') << (int)str[i];
-    return ss.str();
-}
-
 std::string base58_encode(const std::vector<unsigned char>& data) {
-    // Implementasi sederhana dari Base58
     const char* chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
     std::string result;
-    // Langkah encode Base58 disederhanakan untuk demonstrasi
     for (auto byte : data) {
         int carry = byte;
         for (auto& c : result) {
@@ -41,37 +28,21 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Mengambil hexadecimal private key dari argumen baris perintah
-    std::string hex_private_key = argv[1];
-    const char* curve_name = "secp256k1";
-
-    // Setup
-    EVP_PKEY_CTX* pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
-    EVP_PKEY_paramgen_init(pctx);
-    EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, OBJ_sn2nid(curve_name));
-    EVP_PKEY* params = NULL;
-    EVP_PKEY_paramgen(pctx, &params);
-    EVP_PKEY_CTX* kctx = EVP_PKEY_CTX_new(params, NULL);
-    EVP_PKEY_keygen_init(kctx);
-    
-    // Generate key
-    EVP_PKEY* pkey = NULL;
-    EVP_PKEY_keygen(kctx, &pkey);
-
-    // Derive public key
-    unsigned char* pub_key;
-    size_t pub_key_len;
-    EVP_PKEY_get_raw_public_key(pkey, NULL, &pub_key_len);
-    pub_key = (unsigned char*)OPENSSL_malloc(pub_key_len);
-    EVP_PKEY_get_raw_public_key(pkey, pub_key, &pub_key_len);
-
-    // SHA-256
+    // Compute SHA-256
     unsigned char sha256_result[SHA256_DIGEST_LENGTH];
-    SHA256(pub_key, pub_key_len, sha256_result);
+    EVP_MD_CTX* sha256_ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(sha256_ctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(sha256_ctx, argv[1], strlen(argv[1]));
+    EVP_DigestFinal_ex(sha256_ctx, sha256_result, NULL);
+    EVP_MD_CTX_free(sha256_ctx);
 
-    // RIPEMD-160
+    // Compute RIPEMD-160
     unsigned char ripemd_result[RIPEMD160_DIGEST_LENGTH];
-    RIPEMD160(sha256_result, SHA256_DIGEST_LENGTH, ripemd_result);
+    EVP_MD_CTX* ripemd_ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ripemd_ctx, EVP_ripemd160(), NULL);
+    EVP_DigestUpdate(ripemd_ctx, sha256_result, SHA256_DIGEST_LENGTH);
+    EVP_DigestFinal_ex(ripemd_ctx, ripemd_result, NULL);
+    EVP_MD_CTX_free(ripemd_ctx);
 
     // Base58Check Encoding
     std::vector<unsigned char> address_data(ripemd_result, ripemd_result + RIPEMD160_DIGEST_LENGTH);
@@ -79,13 +50,6 @@ int main(int argc, char* argv[]) {
     std::string btc_address = base58_encode(address_data);
 
     std::cout << "Bitcoin Address: " << btc_address << std::endl;
-
-    // Cleanup
-    OPENSSL_free(pub_key);
-    EVP_PKEY_free(pkey);
-    EVP_PKEY_free(params);
-    EVP_PKEY_CTX_free(pctx);
-    EVP_PKEY_CTX_free(kctx);
 
     return 0;
 }
