118d9b2cf182f3b21906ddfc3da3241a45ecd679
Rivaldiananto <rivaldiananto@gmail.com>
2024-07-09 18:55:03 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,220 +1,186 @@
 #include <iostream>
-#include <bitset>
 #include <vector>
-#include <string>
-#include <gmp.h>
-#include <chrono>
-#include <cstdlib>
-#include <sstream>
-#include <iomanip>
+#include <thread>
+#include <mutex>
+#include <unordered_set>
+#include <atomic>
 #include <openssl/sha.h>
-#include <openssl/evp.h>
 #include <openssl/ripemd.h>
-#include <secp256k1.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
 #include <openssl/bn.h>
-#include <algorithm>
-#include <random>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <nlopt.hpp>
 
-// Fungsi untuk mengonversi integer ke string biner 6-bit
-std::string intToBinary6(int num) {
-    std::bitset<6> bin(num);
-    return bin.to_string();
-}
+// Pustaka untuk Base58 encoding perlu ditambahkan atau diimplementasikan
+const char* BASE58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-// Fungsi untuk menghasilkan semua kombinasi biner 6-bit
-std::vector<std::string> generateAllCombinations() {
-    std::vector<std::string> allCombinations;
-    for (int i = 0; i < 64; ++i) {
-        allCombinations.push_back(intToBinary6(i));
-    }
-    return allCombinations;
-}
+// Fungsi Base58 encoding
+std::string base58_encode(const std::vector<unsigned char>& input) {
+    std::vector<unsigned char> temp(input.size() * 2);
+    std::string result;
+    int zero_count = 0;
 
-// Fungsi konversi dari biner ke heksadesimal
-std::string binaryToHex(const std::string &binaryStr) {
-    mpz_t num;
-    mpz_init(num);
-    mpz_set_str(num, binaryStr.c_str(), 2);  // Set num to the value of binaryStr interpreted as a binary number
-    char* hex_cstr = mpz_get_str(NULL, 16, num);  // Convert num to a hexadecimal string
-    std::string hex_str(hex_cstr);
-    free(hex_cstr);
-    mpz_clear(num);
-    
-    // Ensure the hex string is 64 characters long
-    if (hex_str.length() < 64) {
-        hex_str.insert(0, 64 - hex_str.length(), '0');
-    }
-    
-    return hex_str;
-}
-
-// Fungsi tambahan untuk konversi hex ke BTC address
-void hexToBytes(const std::string& hex, std::vector<unsigned char>& bytes) {
-    for (size_t i = 0; i < hex.length(); i += 2) {
-        std::string byteString = hex.substr(i, 2);
-        unsigned char byte = static_cast<unsigned char>(strtol(byteString.c_str(), nullptr, 16));
-        bytes.push_back(byte);
-    }
-}
-
-std::string toBase58Check(const std::vector<unsigned char>& data) {
-    const char* base58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-
-    unsigned char sha256Digest1[SHA256_DIGEST_LENGTH];
-    SHA256(data.data(), data.size(), sha256Digest1);
-    unsigned char sha256Digest2[SHA256_DIGEST_LENGTH];
-    SHA256(sha256Digest1, SHA256_DIGEST_LENGTH, sha256Digest2);
-
-    std::vector<unsigned char> extendedData(data);
-    extendedData.insert(extendedData.end(), sha256Digest2, sha256Digest2 + 4);
-
-    std::vector<unsigned char> base58Encoded;
-    base58Encoded.reserve(extendedData.size() * 138 / 100 + 1);
-
-    BIGNUM* bnData = BN_new();
-    BIGNUM* bnBase = BN_new();
-    BIGNUM* bnMod = BN_new();
-    BN_CTX* ctx = BN_CTX_new();
-
-    BN_bin2bn(extendedData.data(), extendedData.size(), bnData);
-    BN_set_word(bnBase, 58);
-
-    while (!BN_is_zero(bnData)) {
-        BN_div(bnData, bnMod, bnData, bnBase, ctx);
-        unsigned int mod = BN_get_word(bnMod);
-        base58Encoded.push_back(base58Chars[mod]);
-    }
-
-    BN_free(bnData);
-    BN_free(bnBase);
-    BN_free(bnMod);
-    BN_CTX_free(ctx);
-
-    std::reverse(base58Encoded.begin(), base58Encoded.end());
-
-    for (unsigned char byte : data) {
-        if (byte != 0) break;
-        base58Encoded.insert(base58Encoded.begin(), '1');
-    }
-
-    return std::string(base58Encoded.begin(), base58Encoded.end());
-}
-
-// Fungsi baru untuk melakukan hash RIPEMD160 menggunakan EVP
-void RIPEMD160_hash(const unsigned char* input, size_t length, unsigned char* output) {
-    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
-    const EVP_MD* md = EVP_ripemd160();
-    EVP_DigestInit_ex(ctx, md, nullptr);
-    EVP_DigestUpdate(ctx, input, length);
-    EVP_DigestFinal_ex(ctx, output, nullptr);
-    EVP_MD_CTX_free(ctx);
-}
-
-// Fungsi untuk menghasilkan kunci privat acak yang valid
-std::vector<unsigned char> generateRandomPrivateKey() {
-    std::vector<unsigned char> privKey(32);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 255);
-
-    for (int i = 0; i < 32; ++i) {
-        privKey[i] = dis(gen);
-    }
-
-    return privKey;
-}
-
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <number_of_patterns>" << std::endl;
-        return 1;
-    }
-
-    int numPatterns = std::atoi(argv[1]);
-    if (numPatterns <= 0) {
-        std::cerr << "Number of patterns must be positive." << std::endl;
-        return 1;
-    }
-
-    std::vector<std::string> allCombinations = generateAllCombinations();
-
-    mpz_t totalCombinations;
-    mpz_init(totalCombinations);
-    mpz_ui_pow_ui(totalCombinations, 64, numPatterns);  // 64^numPatterns
-
-    gmp_printf("Total kombinasi yang mungkin: %Zd\n", totalCombinations);
-
-    // Start time measurement
-    auto start = std::chrono::steady_clock::now();
-
-    mpz_t i;
-    mpz_init_set_ui(i, 0);
-    unsigned long count = 0;
-    while(mpz_cmp(i, totalCombinations) < 0) {  // Iterate through all combinations
-        unsigned long long combination = mpz_get_ui(i);
-        std::string result;
-        for (int j = 0; j < numPatterns; ++j) {
-            int index = (combination >> (6 * j)) & 0x3F;
-            result += allCombinations[index];
+    for (auto c : input) {
+        if (c == 0x00) {
+            zero_count++;
+        } else {
+            break;
         }
-        std::string hexResult = binaryToHex(result);
-
-        // Debugging output
-        std::cout << "Hex result: " << hexResult << std::endl;
-
-        // Mengubah hexResult menjadi BTC address terkompresi
-        std::vector<unsigned char> privKey = generateRandomPrivateKey();
-
-        secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-        if (ctx == nullptr) {
-            std::cerr << "Failed to create secp256k1 context" << std::endl;
-            return 1;
-        }
-
-        secp256k1_pubkey pubkey;
-        if (!secp256k1_ec_pubkey_create(ctx, &pubkey, privKey.data())) {
-            std::cerr << "Failed to create public key" << std::endl;
-            secp256k1_context_destroy(ctx);
-            mpz_add_ui(i, i, 1);
-            continue;
-        }
-
-        unsigned char pubkeyCompressed[33];
-        size_t outputLength = 33;
-        if (!secp256k1_ec_pubkey_serialize(ctx, pubkeyCompressed, &outputLength, &pubkey, SECP256K1_EC_COMPRESSED)) {
-            std::cerr << "Failed to serialize public key" << std::endl;
-            secp256k1_context_destroy(ctx);
-            mpz_add_ui(i, i, 1);
-            continue;
-        }
-
-        unsigned char sha256Digest[SHA256_DIGEST_LENGTH];
-        SHA256(pubkeyCompressed, outputLength, sha256Digest);
-
-        unsigned char ripemd160Digest[RIPEMD160_DIGEST_LENGTH];
-        RIPEMD160_hash(sha256Digest, SHA256_DIGEST_LENGTH, ripemd160Digest);
-
-        std::vector<unsigned char> extendedRipemd160(21);
-        extendedRipemd160[0] = 0x00;
-        std::copy(ripemd160Digest, ripemd160Digest + RIPEMD160_DIGEST_LENGTH, extendedRipemd160.begin() + 1);
-
-        std::string btcAddress = toBase58Check(extendedRipemd160);
-
-        std::cout << "BTC Address: " << btcAddress << std::endl;
-
-        secp256k1_context_destroy(ctx);
-
-        mpz_add_ui(i, i, 1);
-        count++;
     }
 
-    // End time measurement
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
+    int carry;
+    int j = temp.size() - 1;
 
-    mpz_clear(totalCombinations);
-    mpz_clear(i);
+    for (auto byte : input) {
+        carry = byte;
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            carry += 256 * temp[i];
+            temp[i] = carry % 58;
+            carry /= 58;
+        }
+    }
 
+    auto it = temp.begin();
+    while (it != temp.end() && *it == 0) {
+        it++;
+    }
+
+    for (; it != temp.end(); it++) {
+        result += BASE58_ALPHABET[*it];
+    }
+
+    while (zero_count--) {
+        result.insert(result.begin(), '1');
+    }
+
+    return result;
+}
+
+// Fungsi untuk konversi public key menjadi BTC address
+std::string pubkey_to_address(const std::vector<unsigned char>& pubkey) {
+    unsigned char sha256_1[SHA256_DIGEST_LENGTH];
+    SHA256(pubkey.data(), pubkey.size(), sha256_1);
+
+    unsigned char ripemd160[RIPEMD160_DIGEST_LENGTH];
+    RIPEMD160(sha256_1, SHA256_DIGEST_LENGTH, ripemd160);
+
+    std::vector<unsigned char> hashed_pubkey_with_prefix(1 + RIPEMD160_DIGEST_LENGTH);
+    hashed_pubkey_with_prefix[0] = 0x00; // versi mainnet
+    std::copy(ripemd160, ripemd160 + RIPEMD160_DIGEST_LENGTH, hashed_pubkey_with_prefix.begin() + 1);
+
+    unsigned char sha256_2[SHA256_DIGEST_LENGTH];
+    SHA256(hashed_pubkey_with_prefix.data(), hashed_pubkey_with_prefix.size(), sha256_2);
+
+    unsigned char sha256_3[SHA256_DIGEST_LENGTH];
+    SHA256(sha256_2, SHA256_DIGEST_LENGTH, sha256_3);
+
+    hashed_pubkey_with_prefix.insert(hashed_pubkey_with_prefix.end(), sha256_3, sha256_3 + 4);
+
+    std::string address = base58_encode(hashed_pubkey_with_prefix);
+    return address;
+}
+
+// Fungsi objektif untuk optimasi
+double objective_function(const std::vector<double> &x, std::vector<double> &grad, void *total_value_new) {
+    double total_value = *reinterpret_cast<double*>(total_value_new);
+    double sum_x = 0.0;
+    for (auto val : x) {
+        sum_x += val;
+    }
+    return std::abs(sum_x - total_value);
+}
+
+// Fungsi utama untuk menemukan solusi
+void find_solution(std::unordered_set<std::string>& unique_solutions, std::mutex& solutions_lock, const std::vector<std::string>& target_addresses, std::atomic<bool>& found_event) {
+    double total_value_new = 4.7931547296039625;
+    std::vector<std::pair<double, double>> bounds = { {2.2, 2.8}, {2.2, 2.8} };
+
+    nlopt::opt opt(nlopt::GN_ORIG_DIRECT, 2);
+    std::vector<double> lb = {2.2, 2.2};
+    std::vector<double> ub = {2.8, 2.8};
+    opt.set_lower_bounds(lb);
+    opt.set_upper_bounds(ub);
+    opt.set_min_objective(objective_function, &total_value_new);
+    opt.set_xtol_rel(1e-10);
+
+    while (!found_event.load()) {
+        std::vector<double> x(2);
+        double minf;
+        nlopt::result result = opt.optimize(x, minf);
+        
+        if (result == nlopt::SUCCESS) {
+            std::vector<boost::multiprecision::cpp_dec_float_50> optimal_values_new;
+            for (auto val : x) {
+                optimal_values_new.push_back(boost::multiprecision::cpp_dec_float_50(val));
+            }
+
+            std::vector<boost::multiprecision::cpp_dec_float_50> adjusted_values;
+            for (auto val : optimal_values_new) {
+                adjusted_values.push_back(val + boost::multiprecision::cpp_dec_float_50("19.250411886234843"));
+            }
+
+            std::vector<boost::multiprecision::cpp_dec_float_50> decimal_values;
+            std::vector<std::string> hex_values;
+
+            for (auto val : adjusted_values) {
+                boost::multiprecision::cpp_dec_float_50 log_value = boost::multiprecision::log10(val);
+                decimal_values.push_back(log_value);
+                std::stringstream ss;
+                ss << std::hex << std::setprecision(50) << log_value;
+                hex_values.push_back(ss.str());
+            }
+
+            std::lock_guard<std::mutex> lock(solutions_lock);
+            for (const auto& hex_val : hex_values) {
+                if (unique_solutions.find(hex_val) == unique_solutions.end()) {
+                    unique_solutions.insert(hex_val);
+
+                    std::vector<unsigned char> private_key_bytes(hex_val.begin(), hex_val.end());
+                    EC_KEY *eckey = EC_KEY_new_by_curve_name(NID_secp256k1);
+                    EC_KEY_generate_key(eckey);
+
+                    const EC_POINT *pub_key_point = EC_KEY_get0_public_key(eckey);
+                    BIGNUM *pub_key_bn = BN_new();
+                    EC_POINT_point2bn(EC_KEY_get0_group(eckey), pub_key_point, POINT_CONVERSION_COMPRESSED, pub_key_bn, NULL);
+                    std::vector<unsigned char> pub_key(BN_num_bytes(pub_key_bn));
+                    BN_bn2bin(pub_key_bn, pub_key.data());
+
+                    std::string btc_address = pubkey_to_address(pub_key);
+                    std::cout << "Generated BTC Address: " << btc_address << " for hex value: " << hex_val << std::endl;
+                    if (std::find(target_addresses.begin(), target_addresses.end(), btc_address) != target_addresses.end()) {
+                        std::cout << "Match found! BTC Address: " << btc_address << " for hex value: " << hex_val << std::endl;
+                        found_event.store(true);
+                        break;
+                    }
+
+                    BN_free(pub_key_bn);
+                    EC_KEY_free(eckey);
+                }
+            }
+        } else {
+            std::cout << "Optimization was not successful." << std::endl;
+            break;
+        }
+    }
+}
+
+int main() {
+    std::unordered_set<std::string> unique_solutions;
+    std::mutex solutions_lock;
+    std::vector<std::string> target_addresses = {"1JTK7s9YVYywfm5XUH7RNhHJH1LshCaRFR", "16jY7qLJnxb7CHZyqBP8qca9d51gAjyXQN"};
+    std::atomic<bool> found_event(false);
+    std::vector<std::thread> processes;
+    int num_processes = std::thread::hardware_concurrency();
+
+    for (int i = 0; i < num_processes; ++i) {
+        processes.emplace_back(find_solution, std::ref(unique_solutions), std::ref(solutions_lock), std::ref(target_addresses), std::ref(found_event));
+    }
+
+    for (auto& p : processes) {
+        p.join();
+    }
+
+    std::cout << "All unique solutions found (hexadecimal) are processed." << std::endl;
     return 0;
 }
